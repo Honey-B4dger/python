78de28613caf810cc1887697176585888501207f
Honey-B4dger <toby_hahn85@yahoo.com>
2020-11-13 10:56:06 +0100

    foo

---
+++ misc/prime_bench.py
@@ -0,0 +1,29 @@
+import time
+
+max_range = 10000
+interval = 1000
+max_prime = 2
+
+def is_prime(number):
+    prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            prime = False
+    return prime
+
+if __name__ == '__main__':
+
+    try:
+        start = time.time()
+        for n in range(2,max_range):
+            if is_prime(n):
+                max_prime = n
+            if n% interval ==0:
+                print(f'current max_prime: {max_prime}')
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print('')
+        print(f'maximum range: {max_range}')
+        print(f'final max_prime: {max_prime}')
+        print(f'elapsed time: {round(time.time() - start,1)} s')
