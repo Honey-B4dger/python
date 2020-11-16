c295671d1063ee67f83d2bf9c9c5d93093aeeea0
Honey-B4dger <toby_hahn85@yahoo.com>
2020-11-16 16:11:10 +0100

    foo

---
+++ alfa/aufgabe_2_2.py
@@ -0,0 +1,8 @@
+a = 29
+b = 7
+
+c = b / a
+
+result = round(c, 2)
+
+print(f'Das Ergebnis lautet: {result}')
---
+++ alfa/aufgabe_2_3.py
@@ -0,0 +1,6 @@
+try:
+meinname = input('Bitte gib deinen namen an!')
+
+text1 = f'Python\'s Interpreter sagt Hallo zu: {meinname}'
+
+print(text1 + meinname)
