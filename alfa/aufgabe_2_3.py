bd7a7de1be2b122c65167223c58f4e8a24d9d7ee
Honey-B4dger <toby_hahn85@yahoo.com>
2020-11-16 17:00:39 +0100

    foo

--- alfa/aufgabe_2_3.py
+++ alfa/aufgabe_2_3.py
@@ -1,6 +1,5 @@
-try:
-meinname = input('Bitte gib deinen namen an!')
+meinname = input('Bitte gib deinen namen an: ')
 
 text1 = f'Python\'s Interpreter sagt Hallo zu: {meinname}'
 
-print(text1 + meinname)
+print(text1)
