4e507d468844b93e0adcbfd160d0d9a5f963156c
Toby <toby@mac-mini.local>
2020-12-04 19:14:13 +0100

    foo

--- alfa/projekt/sudoku_3d_plotter.py
+++ alfa/projekt/sudoku_3d_plotter.py
@@ -19,14 +19,13 @@ with open(file) as f:
             data[labels[column]].append(int(value))
 
 fig = plt.figure()
-
 ax = plt.axes(projection='3d')
 
 rows = data['row']
 columns = data['column']
 iterations = data['iteration']
 
-ax.scatter3D(rows, columns, iterations, c=iterations, cmap=cm.coolwarm)
+ax.plot3D(rows, columns, iterations, c=iterations, cmap=cm.rainbow, alpha=.5)
 ax.set_xlabel('row')
 ax.set_ylabel('column')
 ax.set_zlabel('iteration')
